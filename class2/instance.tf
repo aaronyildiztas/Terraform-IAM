74ce9661273f36090dceada5adfdfdd0e776faca
Aaron Yildiztas <yildiztasmusa@gmail.com>
2021-02-05 03:07:01 +0000

    v1

--- class2/instance.tf
+++ class2/instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "ami-047a51fa27710816e"
-  instance_type = "t2.micro"
-  count = 1
-
+  ami                         = "ami-047a51fa27710816e"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = "true"
+  key_name = "${aws_key_pair.developer.key_name}"
   tags = {
     Name = "class2"
   }
